771fac254fc2d10cba3b04e8bfda726a0e7791e8
Nazira-umucyo <n.umucyo@alustudent.com>
2025-07-09 18:25:49 +0200

    yess

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -13,26 +13,21 @@ def top_ten(subreddit):
     If the subreddit is invalid, prints None.
     """
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:reddit.api.advanced:v1.0.0 (by /u/student)"
-    }
+    headers = {"User-Agent": "MyRedditApp/0.1"}
+    params = {"limit": 10}
+    
+    response = requests.get(url, headers=headers, params=params,
+                          allow_redirects=False)
+    
+    if response.status_code != 200:
+        print(None)
+        return
     
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-        if response.status_code == 200:
-            data = response.json()
-            
-            if 'data' in data and 'children' in data['data']:
-                posts = data['data']['children']
-                
-                for post in posts[:10]:
-                    if 'data' in post and 'title' in post['data']:
-                        print(post['data']['title'])
-            else:
-                print(None)
-        else:
-            print(None)
-            
+        data = response.json().get("data", {}).get("children", [])
+        for post in data:
+            title = post.get("data", {}).get("title")
+            if title:
+                print(title)
     except Exception:
         print(None)
