6236c09ba97450eeb02f534185de6d1efbd2f4fc
Nazira-umucyo <n.umucyo@alustudent.com>
2025-07-09 14:39:24 +0000

    yess

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,12 +1,25 @@
 #!/usr/bin/python3
-"""
-1-main
-"""
-import sys
+"""Fetches and prints titles of the first 10 hot posts on a subreddit."""
 
-if __name__ == '__main__':
-    top_ten = __import__('1-top_ten').top_ten
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        top_ten(sys.argv[1])
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts.
+    If subreddit is invalid, prints None.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {"User-Agent": "CustomUserAgent/1.0"}
+
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code != 200:
+            print("None")
+            return
+
+        posts = response.json().get("data", {}).get("children", [])
+        for post in posts:
+            print(post.get("data", {}).get("title"))
+    except Exception:
+        print("None")
